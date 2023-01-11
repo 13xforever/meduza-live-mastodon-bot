e59da37dbbe4d975c2f64cc646388cf2c396e949
13xforever <ilya.veselov@gmail.com>
2023-01-12 01:54:25 +0500

    update important regex

--- MeduzaRepost/MastodonWriter.cs
+++ MeduzaRepost/MastodonWriter.cs
@@ -20,6 +20,7 @@ public sealed class MastodonWriter: IObserver<TgEvent>, IDisposable
                     ((подходит\s+к\s+концу|завершается).+день)
                     |((принят|подписа[лн]|одобр(ил|ен)|внес(ен|ли)).+закон)
                     |(главные\s+(фото(графии)?|события))
+                    |(призыв|мобилизаци[ия])
                 ))
                 """,
         RegexOptions.IgnoreCase | RegexOptions.Multiline | RegexOptions.ExplicitCapture | RegexOptions.Compiled | RegexOptions.IgnorePatternWhitespace
