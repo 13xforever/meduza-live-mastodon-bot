5a068e63ec40e9a69ed1c9aee124cc6b5973dd2f
13xforever <ilya.veselov@gmail.com>
2023-11-17 19:47:19 +0500

    try to make error content logging actually work

--- MeduzaRepost/Mastodon/CustomMastodonClient.cs
+++ MeduzaRepost/Mastodon/CustomMastodonClient.cs
@@ -1,9 +1,12 @@
-﻿using Mastonet;
+﻿using System.Text;
+using Mastonet;
 
 namespace MeduzaRepost;
 
 public class CustomMastodonClient:MastodonClient
 {
+    private static readonly Encoding Utf8 = new UTF8Encoding(false);
+    
     public string? LastErrorResponseContent { get; private set; }
     
     public CustomMastodonClient(string instance, string accessToken) : base(instance, accessToken)
@@ -21,7 +24,10 @@ public class CustomMastodonClient:MastodonClient
         }
         catch
         {
-            LastErrorResponseContent = response.Content.ReadAsStringAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+            using var bufferCopy = new MemoryStream();
+            response.Content.CopyTo(bufferCopy, null, CancellationToken.None);
+            bufferCopy.Seek(0, SeekOrigin.Begin);
+            LastErrorResponseContent = Utf8.GetString(bufferCopy.ToArray());
             throw;
         }
     }
