452959b10882c1c57e8a0abe20599cda75ba85c2
13xforever <ilya.veselov@gmail.com>
2024-06-09 11:59:36 +0500

    add a watchdog hack to workaround current telegram issues

--- MeduzaRepost/Program.cs
+++ MeduzaRepost/Program.cs
@@ -11,18 +11,22 @@ static void Restart()
 
 try
 {
-    Config.Log.Debug("Upgrading databases...");
+    Config.Log.Debug("Upgrading databases…");
     await DbImporter.UpgradeAsync(Config.Cts.Token).ConfigureAwait(false);
 
-    Config.Log.Debug("Creating telegram reader...");
+    Config.Log.Debug("Creating telegram reader…");
     using var reader = new TelegramReader();
-    Config.Log.Debug("Creating mastodon writer...");
+    Config.Log.Debug("Creating mastodon writer…");
     using var writer = new MastodonWriter();
 
-    Config.Log.Debug("Running mastodon writer...");
+    Config.Log.Debug("Creating watchdog…");
+    using var watchdog = new Watchdog(Restart);
+    using var watchdogSub = reader.Subscribe(watchdog);
+
+    Config.Log.Debug("Running mastodon writer…");
     var writerTask = writer.Run(reader);
-    using var unsubscriber = reader.Subscribe(writer);
-    Config.Log.Debug("Running telegram reader...");
+    using var mastodonSub = reader.Subscribe(writer);
+    Config.Log.Debug("Running telegram reader…");
     await reader.Run().ConfigureAwait(false);
     await writerTask.ConfigureAwait(false);
     if (!Config.Cts.IsCancellationRequested)
---
+++ MeduzaRepost/Watchdog.cs
@@ -0,0 +1,55 @@
+﻿namespace MeduzaRepost;
+
+public class Watchdog: IObserver<TgEvent>, IDisposable
+{
+    private readonly Action onTimeout;
+    private Task trigger;
+    private CancellationTokenSource cts = new();
+    private readonly object syncObj = new();
+
+    public Watchdog(Action onTimeout)
+    {
+        this.onTimeout = onTimeout;
+        var token = cts.Token;
+        trigger = Task.Delay(Config.WatchdogThreshold, token).ContinueWith(_ =>
+        {
+            if (!token.IsCancellationRequested)
+                this.onTimeout();
+        }, token);
+    }
+
+    public void Reset()
+    {
+        lock (syncObj)
+        {
+            cts.Cancel(false);
+            if (!cts.TryReset())
+            {
+                cts.Dispose();
+                cts = new();
+            }
+            trigger.Dispose();
+            var token = cts.Token;
+            trigger = Task.Delay(Config.WatchdogThreshold, token).ContinueWith(_ =>
+            {
+                if (!token.IsCancellationRequested)
+                    onTimeout();
+            }, token);
+        }
+    }
+
+    public void Dispose()
+    {
+        cts.Cancel(false);
+        trigger.Dispose();
+        cts.Dispose();
+    }
+
+    public void OnCompleted() => Dispose();
+    public void OnError(Exception error) {}
+    public void OnNext(TgEvent value)
+    {
+        if (value.Type is TgEventType.Post or TgEventType.Edit or TgEventType.Delete or TgEventType.Pin)
+            Reset();
+    }
+}
